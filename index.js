0555370e96ecd75d38c250ac1b384111d0746138
Olga Naboichenko <olga140404ol@gmail.com>
2022-06-14 16:06:03 +0300

    home-task-10

---
+++ index.js
@@ -0,0 +1,21 @@
+function askForInput(){
+    let words = [];
+    while(true){
+        const message = prompt('Enter your word');
+
+        if (message === "" || message === null) break;
+
+        words.push(message);
+    }
+
+   const upperCasedArray = words.map((string) =>{
+        return `${string[0].toUpperCase()}`;  
+   });
+   
+   const sortedArray = upperCasedArray.sort((first, second) =>{
+       return first.length - second.length;
+   });
+    return sortedArray;
+}
+askForInput();
+console.log(askForInput());
